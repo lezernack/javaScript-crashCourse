1007da7b5784222675fae456cf7015579c0fd212
lezernack <luks.ezck@gmail.com>
2024-01-30 14:33:47 -0700

    changed index and css

--- script.js
+++ script.js
@@ -1,28 +1,2 @@
-function Person(firstName, lastName, dob) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.dob = dob;
-}
 
-// Class
-class Person {
-  constructor(firstName, lastName, dob) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.dob = new Date(dob);
-  }
 
-  getBirthYear() {
-    return this.dob.getFullYear();
-  }
-
-  getFullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-}
-
-//Istantiate object
-const person1 = new Person("John", "Doe", "4-3-1980");
-const person2 = new Person("Mary", "Smith", "3-6-1970");
-
-console.log(person2.firstName);
